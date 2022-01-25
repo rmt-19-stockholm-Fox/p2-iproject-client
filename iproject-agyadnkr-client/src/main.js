9a032e58c082adca5b565981b27f21712fec883c
agyadnkr <agyad.dinakara@gmail.com>
2022-01-25 21:20:35 +0700

    add google maps html

--- iproject-agyadnkr-client/src/main.js
+++ iproject-agyadnkr-client/src/main.js
@@ -3,10 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBjlQ0LbPIFIH3rExuCRIFoDifRyNAyenw",
+    libraries: "places"
+  }
+});
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
+
