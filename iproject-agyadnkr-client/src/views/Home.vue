d41acdd570e632291aecb35b722feb4f039c8270
agyadnkr <agyad.dinakara@gmail.com>
2022-01-25 22:06:25 +0700

    Add component GoogleMaps and show it to home

--- iproject-agyadnkr-client/src/views/Home.vue
+++ iproject-agyadnkr-client/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <GoogleMaps />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import GoogleMaps from '../components/GoogleMaps.vue'
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    GoogleMaps
   },
 };
 </script>
