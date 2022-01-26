e7d91c2d981da7f8083a2efc17aa23da18dd06f2
Daniel Levyson <levyson.daniel@gmail.com>
2022-01-26 19:08:49 +0700

    display place's name on Post card

--- src/main.js
+++ src/main.js
@@ -9,9 +9,19 @@ import GAuth from 'vue-google-oauth2';
 import { initializeApp } from 'firebase/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faPlus, faAngleLeft, faAngleRight, faBraille } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlus,
+  faAngleLeft,
+  faAngleRight,
+  faBraille,
+  faMapMarkerAlt
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faPlus, faAngleLeft, faAngleRight, faBraille);
+library.add(
+  faPlus, faAngleLeft, 
+  faAngleRight, faBraille,
+  faMapMarkerAlt
+);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 initializeApp({
