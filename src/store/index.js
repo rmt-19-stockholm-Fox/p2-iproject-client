1d20237197b27f193c690e0a0d13470fabc03262
Bimo AR <bimoarta@gmail.com>
2022-01-28 10:18:16 +0700

    is this done

--- src/store/index.js
+++ src/store/index.js
@@ -9,9 +9,132 @@ export default new Vuex.Store({
     server: `http://localhost:3000/`,
     start: false,
     start2: false,
+    start3: false,
     perftime: false,
     perfplace: false,
-    country: [`Afghanistan`, `Albania`, ``],
+    perfhotel: false,
+    done: false,
+    country: [
+      `Afghanistan`,
+      `Albania`,
+      `Algeria`,
+      `Argentina`,
+      `Australia`,
+      `Austria`,
+      `Bangladesh`,
+      `Belgium`,
+      `Bhutan`,
+      `Bolivia`,
+      `Botswana`,
+      `Brazil`,
+      `Brunei`,
+      `Bulgaria`,
+      `Cambodia`,
+      `Canada`,
+      `Chile`,
+      `China`,
+      `Colombia`,
+      `Congo`,
+      `Croatia`,
+      `Cuba`,
+      `Czech Republic`,
+      `Denmark`,
+      `Dominica`,
+      `Ecuador`,
+      `Egypt`,
+      `Estonia`,
+      `Ethiopia`,
+      `Finland`,
+      `France`,
+      `Gabon`,
+      `Georgia`,
+      `Germany`,
+      `Gibraltar`,
+      `Greece`,
+      `Greenland`,
+      `Guatemala`,
+      `Haiti`,
+      `Hong Kong`,
+      `Hungary`,
+      `Iceland`,
+      `India`,
+      `Indonesia`,
+      `Iran`,
+      `Iraq`,
+      `Ireland`,
+      `Israel`,
+      `Italy`,
+      `Jamaica`,
+      `Japan`,
+      `Kazakhstan`,
+      `Kenya`,
+      `Kuwait`,
+      `Kyrgyzstan`,
+      `Laos`,
+      `Latvia`,
+      `Lebanon`,
+      `Libya`,
+      `Lithuania`,
+      `Madagascar`,
+      `Malaysia`,
+      `Maldives`,
+      `Mexico`,
+      `Mongolia`,
+      `Montenegro`,
+      `Morocco`,
+      `Myanmar`,
+      `Namibia`,
+      `Nepal`,
+      `Netherlands`,
+      `New Zealand`,
+      `Nigeria`,
+      `North Korea`,
+      `North Macedonia`,
+      `Norway`,
+      `Pakistan`,
+      `Panama`,
+      `Paraguay`,
+      `Peru`,
+      `Philippines`,
+      `Poland`,
+      `Portugal`,
+      `Puerto Rico`,
+      `Qatar`,
+      `Romania`,
+      `Russia`,
+      `Samoa`,
+      `Saudi Arabia`,
+      `Serbia`,
+      `Singapore`,
+      `Slovakia`,
+      `Somalia`,
+      `South Africa`,
+      `South Korea`,
+      `Spain`,
+      `Sri Lanka`,
+      `Sweden`,
+      `Switzerland`,
+      `Syria`,
+      `Taiwan`,
+      `Tanzania`,
+      `Thailand`,
+      `Turkey`,
+      `Uganda`,
+      `Ukraine`,
+      `United Kingdom`,
+      `United States`,
+      `Uruguay`,
+      `Uzbekistan`,
+      `Venezuela`,
+      `Vietnam`,
+      `Yemen`,
+      `Zambia`,
+      `Zimbabwe`,
+    ],
+    selecteddates: [],
+    selectedplace: {},
+    selectadd: "",
+    selectedhotel: {},
   },
   mutations: {
     CHANGE_START(state, plod) {
@@ -20,11 +143,40 @@ export default new Vuex.Store({
     CHANGE_START2(state, plod) {
       state.start2 = plod;
     },
+    CHANGE_START3(state, plod) {
+      state.start3 = plod;
+    },
+    CHANGE_DONE(state, plod) {
+      state.done = plod;
+    },
     CHANGE_PERFTIME(state, plod) {
       state.perftime = plod;
     },
     CHANGE_PERFPLACE(state, plod) {
       state.perfplace = plod;
+    },
+    CHANGE_PERFHOTEL(state, plod) {
+      state.perfhotel = plod;
+    },
+    CHANGE_SELECTEDD(state, plod) {
+      state.selecteddates = plod;
+    },
+    CHANGE_SELECTEDP(state, plod) {
+      if (plod == {}) {
+        state.selectedplace = plod;
+      } else {
+        state.selectedplace = plod;
+      }
+    },
+    CHANGE_SELECTEDH(state, plod) {
+      if (plod == {}) {
+        state.selectedhotel = plod;
+      } else {
+        state.selectedhotel = plod;
+      }
+    },
+    CHANGE_ADD(state, plod) {
+      state.selectadd = plod;
     },
   },
   actions: {
@@ -45,7 +197,22 @@ export default new Vuex.Store({
       try {
         console.log(plod);
         const resu = await axios.post(`${this.state.server}findplace`, plod);
+        console.log(resu.data);
         context.commit(`CHANGE_PERFPLACE`, resu.data);
+      } catch (error) {
+        Vue.swal({
+          icon: "error",
+          title: "Something went wrong!",
+          text: "Make sure you have filled the forms correctly.",
+        });
+      }
+    },
+    async getmyhotel(context, plod) {
+      try {
+        console.log(plod);
+        const resu = await axios.post(`${this.state.server}findhotel`, plod);
+        console.log(resu.data);
+        context.commit(`CHANGE_PERFHOTEL`, resu.data);
       } catch (error) {
         Vue.swal({
           icon: "error",
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
           <form @submit.prevent="getmydays" class="rojust">
             <div class="form-group">
          <label for="country">Country:</label>
-         <input list="countryname" id="country" name="country" class="form-control text-muted" v-model="country" placeholder="The country you live in (this will determine your national holidays)">
+         <input list="countryname" id="country" name="country" class="form-control text-muted" v-model="country" placeholder="The country you live in (this will determine your national holidays).">
          <datalist id="countryname">
               <option v-for="x in storecount" :key="x">{{x}}</option>
           </datalist>
@@ -14,81 +14,162 @@
             <br>
             <div class="form-group">
         <label for="year">Year:</label>
-         <input type="number" id="year" name="year" class="form-control text-muted" v-model="year" min="2020" placeholder="2020"><br>
+         <input type="number" id="year" name="year" class="form-control text-muted" v-model="year" min="2020"><br>
                 </div>
                 <div class="form-group">
         <label for="pleave">Paid-leaves:</label>
-         <input type="number" id="pleave" name="pleave" class="form-control text-muted" v-model="forcednum" min="0" placeholder="Number of paid-leaves you are willing to make for the perfect holiday">
-         <small class="form-text text-muted">*)Required</small>
+         <input type="number" id="pleave" name="pleave" class="form-control text-muted" v-model="forcednum" min="0" placeholder="Number of paid-leaves you are willing to make.">
+         <small class="form-text text-muted">*)Must only contain numbers, default: 0</small>
                 </div>
                 <br>
                 <div class="form-group">
         <label class="form-check-label">Weekly day-off(s):</label>
         <br>
         <label class="form-check-label" for="exampleCheck1">MON:</label>
-         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="mon" id="exampleCheck1">
+         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="Mon" id="exampleCheck1">
          <label class="form-check-label" for="exampleCheck2">.  TUE:</label>
-         <input type="checkbox" class="form-check-input" v-model="nmdayoff" value="tue" id="exampleCheck2">
-         <label class="form-check-label" for="exampleCheck2">.  WED:</label>
-         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="wed" id="exampleCheck3">
-         <label class="form-check-label" for="exampleCheck2">.  THU:</label>
-         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="thu" id="exampleCheck4">
-         <label class="form-check-label" for="exampleCheck2">.  FRI:</label>
-         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="fri" id="exampleCheck5"> 
-         <label class="form-check-label" for="exampleCheck2">.  SAT:</label>
-         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="sat" id="exampleCheck6"> 
-         <label class="form-check-label" for="exampleCheck2">.  SUN:</label>
-         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="sun" id="exampleCheck7">
+         <input type="checkbox" class="form-check-input" v-model="nmdayoff" value="Tue" id="exampleCheck2">
+         <label class="form-check-label" for="exampleCheck3">.  WED:</label>
+         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="Wed" id="exampleCheck3">
+         <label class="form-check-label" for="exampleCheck4">.  THU:</label>
+         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="Thu" id="exampleCheck4">
+         <label class="form-check-label" for="exampleCheck5">.  FRI:</label>
+         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="Fri" id="exampleCheck5"> 
+         <label class="form-check-label" for="exampleCheck6">.  SAT:</label>
+         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="Sat" id="exampleCheck6"> 
+         <label class="form-check-label" for="exampleCheck7">.  SUN:</label>
+         <input type="checkbox" class="form-check-input" v-model="nmdayoff"  value="Sun" id="exampleCheck7">
                 </div><br><br><br>
                 <button v-if="getperftime===false" type="submit" class="btn btn-primary">Submit</button>
-                 <button v-else type="submit" class="btn btn-primary">Change My Inputs</button>
+                 <button v-else type="submit" class="btn btn-primary">Change My Inputs</button><br><br><br><br><br>
           </form> 
         </div>
       
-      <div v-show="getperftime!==false">
+      <div v-if="getperftime!==false&&getstatstart2===false">
+      <h1 v-if="getperftime!==false&&getstatstart2===false">Here are your possible streaks:</h1>
         <div class="container2 column icon-boxes">
-        <div v-for="mama in getperftime" :key="mama.dates[0][0]" class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+        <div v-for="mama in getperftime" :key="mama.dates[0]" class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
           <div class="icon-box">
             <div class="icon"><i class="ri-stack-line"></i></div>
-            <h4 class="title">Holiday Streaks In</h4>
             <div  v-for="anak in mama" :key="anak[0]">
             <p v-for="cucu in anak" :key="cucu">{{cucu}}</p>
+             <button @click="start2(anak)" v-show="getperftime!==false" type="button" class="btn btn-get-started scrollto">Go to next step with these dates</button>
+             </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+      <div v-if="getstatstart2===true">
+        <h1>HERE'S YOUR HOLIDAY</h1>
+        <div class="container2 column icon-boxes">
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-stack-line"></i></div>
+            <div>
+             <h4 class="title">Your Dates:</h4>
+            <p v-for="er in this.selecteddates" :key="er">{{er}}</p>
             </div>
           </div>
         </div>
-        <button @click="start2" v-show="getperftime!==false" type="button" class="btn btn-get-started scrollto">Find me intersesting places to visit!</button>
+
+        <div v-if="getstatstart3!==false" class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-stack-line"></i></div>
+            <h4 class="title">Place you'd like to visit:</h4>
+            <p>{{selectedplace.wikipedia_extracts.text}}</p>
+            <a :href="selectedplace.wikipedia">Read more on Wikipedia</a>
+          </div>
+        </div>
+
+        <div v-if="done===true" class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-stack-line"></i></div>
+            <img :src="selectedhotel.optimizedThumbUrls.srpDesktop">
+            <h4 class="title">{{selectedhotel.name}}</h4>
+            <p>{{selectedhotel.address.streetAddress}}</p>
+            <p>Star: {{selectedhotel.starRating}}</p>
+            <p>Guest score: {{selectedhotel.guestReviews.rating}}</p>
+          </div>
+        </div>
+
       </div>
       </div>
+                <br>
+                 <br>
+                  <br>
 
 
-       <form @submit.prevent="getmyplaces" class="rojust" v-if="getstatstart2!==false">
+       <form @submit.prevent="getmyplaces" class="rojust" v-if="getstatstart2!==false && getstatstart3===false">
             <div class="form-group">
-         <label for="place">Place:</label>
-         <input type="text" id="place" name="place" class="form-control text-muted" v-model="place" placeholder="City or country you will spend your holidays in"><br>
+         <label for="place">City: </label>
+         <input type="text" id="place" name="place" class="form-control text-muted" v-model="place" placeholder="Where you would like to spend your holidays in.">
+          <small class="form-text text-muted">*)Required</small><br>
             </div>
             <div class="form-group">
         <label for="mdist">Max Travel Distance:</label>
-         <input type="number" id="mdist" name="mdist" class="form-control text-muted" v-model="mdist" min="0" placeholder="Maximum kilometer outside of the city you are willing to go through (not applicable if you choose country in the above form)"><br>
+         <input type="number" id="mdist" name="mdist" class="form-control text-muted" v-model="mdist" min="0" placeholder="from outside your chosen city (in Kilometers).">
+         <small class="form-text text-muted">*)Must only contain numbers, default: 0</small><br><br>
+
                 </div>
                 <button v-if="getperftime!==false&&getperfplace===false" type="submit" class="btn btn-primary">Submit</button>
                  <button v-if="getperftime!==false&&getperfplace!==false" type="submit" class="btn btn-primary">Find Me Another Place</button>
           </form>
           
+<br><br>
 
-
-           <div v-show="getperfplace!==false">
+           <div v-if="getperfplace!==false&&getstatstart3===false">
         <div class="container2 column icon-boxes">
-        <div v-for="bapak in getperfplace" :key="bapak.xid" class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+        <div v-for="ayo in getperfplace" :key="ayo.xid" class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
           <div class="icon-box">
             <div class="icon"><i class="ri-stack-line"></i></div>
-            <h4 class="title">{{bapak.name}}</h4>
-            <p>{{bapak.wikipedia_extracts.texts}}</p>
-            <img :src="bapak.image">
+            <h4 class="title">{{ayo.name}}</h4>
+            <p>{{ayo.wikipedia_extracts.text}}</p>
+            <a :href="ayo.wikipedia">Read more on Wikipedia</a>
+             <button @click="start3(ayo)" type="button" class="btn btn-get-started scrollto">Find hotels near this place</button>
           </div>
         </div>
       </div>
       </div>
 
+      <form @submit.prevent="getmyhotel" class="rojust" v-if="getstatstart3===true&&done===false">
+            <div class="form-group">
+        <label for="star">Hotel star rating:</label>
+         <input type="number" id="star" name="star" class="form-control text-muted" v-model="star" placeholder="1 to 6, if empty this will be defaulted to any stars.">
+          <small class="form-text text-muted">*)Must only contain numbers, default: 3</small><br><br>
+         <label for="mgrating">Minimum guest ratings:</label>
+         <input type="number" id="mgrating" name="mgrating" class="form-control text-muted" v-model="mgrating" placeholder="1 to 10, also accepts up to one decimal">
+          <small class="form-text text-muted">*)Must only contain numbers, default: 5</small><br><br>
+         <label for="maxprice">Maximum price:</label>
+         <input type="number" id="maxprice" name="maxprice" class="form-control text-muted" v-model="maxprice" placeholder="for a room for 1 person. (in USD)"> 
+         <small class="form-text text-muted">*)Must only contain numbers, default: 500</small><br><br>
+
+                </div>
+                <button v-if="getstatstart3!==false&&getperfhotel===false" type="submit" class="btn btn-primary">Submit</button>
+                 <button v-if="getstatstart3!==false&&getperfhotel!==false" type="submit" class="btn btn-primary">Change My Inputs</button>
+          </form>
+
+          <div v-if="perfhotel!==false&&done===false">
+            <br>
+            <br>
+            <br>
+            <br>
+            <h1>Select your prefered hotel to finalize your choises</h1>
+        <div class="container2 column icon-boxes">
+        <div v-for="kemon in perfhotel" :key="kemon.id" class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-stack-line"></i></div>
+            <img :src="kemon.optimizedThumbUrls.srpDesktop">
+            <h4 class="title">{{kemon.name}}</h4>
+            <p>{{kemon.address.streetAddress}}</p>
+            <p>Star: {{kemon.starRating}}</p>
+            <p>Guest score: {{kemon.guestReviews.rating}}</p>
+             <button @click="start4(kemon)" type="button" class="btn btn-get-started scrollto">Select</button>
+          </div>
+        </div>
+      </div>
+      </div>
 
       
     </div>
@@ -102,9 +183,13 @@ export default {
       country : "",
       year: 2020,
       forcednum: "",
-      nmdayoff: [`sat`, `sun`],
+      nmdayoff: [`Sat`, `Sun`],
       mdist: "",
-      place: ""
+      place: "",
+      maxprice: "",
+      mgrating: "",
+      star: "",
+      last: {}
     }
   },
   methods: {
@@ -117,8 +202,30 @@ export default {
       }
       await this.$store.dispatch(`getmydays`, plod)
     },
-    start2(){
+    start2(anak){
+      this.$store.commit(`CHANGE_SELECTEDD`, anak)
       this.$store.commit(`CHANGE_START2`, true)
+      localStorage.dates = anak
+    },
+    start3(ayo){
+      this.$store.commit(`CHANGE_SELECTEDP`, ayo)
+      this.$store.commit(`CHANGE_START3`, true)
+      localStorage.placesjs = ayo
+      if (ayo.address.pedestrian) {
+       localStorage.places = ayo.address.pedestrian
+       this.$store.commit(`CHANGE_ADD`, ayo.address.pedestrian) 
+      }else if (ayo.address.city) {
+        localStorage.places = ayo.address.city
+       this.$store.commit(`CHANGE_ADD`, ayo.address.city) 
+      }else {
+        localStorage.places = ayo.address.state
+       this.$store.commit(`CHANGE_ADD`, ayo.address.state) 
+      }
+    },
+    start4(nyem){
+      console.log(nyem);
+       this.$store.commit(`CHANGE_SELECTEDH`, nyem)
+      this.$store.commit(`CHANGE_DONE`, true)
     },
     async getmyplaces(){
       const plod = {
@@ -126,6 +233,15 @@ export default {
         maxdistance: this.mdist,
       }
       await this.$store.dispatch(`getmyplaces`, plod)
+    },
+    async getmyhotel(){
+      const plod = {
+        maxprice:this.maxprice,
+        mgrating:this.mgrating,
+        star:this.star,
+        strname:this.selectadd
+      }
+      await this.$store.dispatch(`getmyhotel`, plod)
     },
   },
   computed: {
@@ -135,33 +251,50 @@ export default {
     getstatstart2() {
       return this.$store.state.start2
     },
+    getstatstart3() {
+      return this.$store.state.start3
+    },
     getperftime() {
       return this.$store.state.perftime
     },
     getperfplace() {
       return this.$store.state.perfplace
     },
+    getperfhotel() {
+      return this.$store.state.perfhotel
+    },
     storecount() {
       return this.$store.state.country
-    }
+    },
+    selecteddates(){
+      return this.$store.state.selecteddates
+    },
+    selectedplace(){
+      return this.$store.state.selectedplace
+    },
+    selectedhotel(){
+      return this.$store.state.selectedhotel
+    },
+    selectadd(){
+      return this.$store.state.selectadd
+    },
+    perfhotel(){
+return this.$store.state.perfhotel
+    },
+    done(){
+return this.$store.state.done
+    },
   } 
 }
 </script>
 
 
 <style>
-.rowjust {
-  width: 20vw;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-}
 
 .container2 {
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: center;
   flex-wrap: wrap;
 }
 
